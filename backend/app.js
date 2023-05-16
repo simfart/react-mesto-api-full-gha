53ff5b6278a60ead0a77b9493e51bc8c66e361a7
Artjugina <simf.i@ya.ru>
2023-05-17 01:14:57 +0300

    cors-2

--- backend/app.js
+++ backend/app.js
@@ -11,22 +11,21 @@ const { PORT, MONGO_ADRESS } = require('./utils/config');
 
 const app = express();
 mongoose.connect(MONGO_ADRESS);
-app.use(cors());
+// app.use(cors());
 
-// app.use(cors({
-//   origin: [
-//     'http://84.201.128.134',
-//     'http://84.201.128.134:3000',
-//     'http://84.201.128.134:3001',
-//     'http://localhost:3001',
-//     'http://localhost:3000',
-//     'http://127.0.0.1:27017',
-//     'http://simfart.nomoredomains.monster',
-//     'https://simfart.nomoredomains.monster',
-//   ],
-//   method: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-//   credentials: true,
-// }));
+app.use(cors({
+  origin: [
+    'http://84.201.128.134',
+    'http://84.201.128.134:3000',
+    'http://84.201.128.134:3001',
+    'http://localhost:3001',
+    'http://127.0.0.1:27017',
+    'http://simfart.nomoredomains.monster',
+    'https://simfart.nomoredomains.monster',
+  ],
+  method: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+  credentials: true,
+}));
 
 app.use(requestLogger); // подключаем логгер запросов
 app.use(express.json());
