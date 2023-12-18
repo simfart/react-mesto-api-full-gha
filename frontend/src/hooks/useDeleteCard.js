a11e6e79eb679d1335429630310847661b00e5b1
thrmdyy <thrmdy@gmail.com>
2023-12-18 22:24:25 +0500

    removed unnessesary useRef

--- frontend/src/hooks/useDeleteCard.js
+++ frontend/src/hooks/useDeleteCard.js
@@ -2,12 +2,9 @@ import { useMutation, useQueryClient } from 'react-query'
 import { deleteCard } from '../api'
 import { usePopups } from './usePopups'
 import { useMemo } from 'react'
-import { useRef } from 'react'
 
 export const useDeleteCard = () => {
   const { closePopup } = usePopups()
-  //
-  const { data: card } = useRef()
   const queryClient = useQueryClient()
 
   const { mutate, isLoading } = useMutation(deleteCard, {
--- frontend/src/hooks/useLikeCard.js
+++ frontend/src/hooks/useLikeCard.js
@@ -1,10 +1,8 @@
 import { useMutation, useQueryClient } from 'react-query'
 import { toggleCardLike } from '../api'
 import { useMemo } from 'react'
-import { useRef } from 'react'
 
 export const useLikeCard = () => {
-  const { data: card } = useRef()
   const queryClient = useQueryClient()
 
   const { mutate, isLoading } = useMutation(toggleCardLike, {
